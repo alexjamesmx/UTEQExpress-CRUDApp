57a058ea019bd70967f814c48d0d8c58056f2dab
Alex Santiago <sg.alex32@gmail.com>
2023-11-30 13:31:53 -0600

    gitignore

--- database/db.js
+++ database/db.js
@@ -1,19 +1,30 @@
-﻿const { Sequelize } = require("sequelize");
+﻿const { Sequelize } = require("sequelize")
 
 //conectar a la BD
-const sequelize = new Sequelize('web_integral', 'root', process.env.BD_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+const sequelize = new Sequelize(
+  process.env.BD_NAME,
+  process.env.USERNAME,
+  process.env.BD_PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    },
+  }
+)
 
 const open = async () => {
-    await sequelize.authenticate().then(
-      console.log("Conectado a la BD MySQL local.")
-      ).catch((error) => {
+  await sequelize
+    .authenticate()
+    .then(console.log("Conectado a la BD MySQL local."))
+    .catch((error) => {
       console.log("Error de conexión:", error)
     })
-  }
+}
 
-  open();
+open()
 
-module.exports = sequelize;
+module.exports = sequelize
